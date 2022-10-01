3ed0e37a5f86f73b443913005af557bb6ac6f400
nikolas <nikolasoliveira883@gmail.com>
2022-09-30 22:23:21 -0300

    projetofinalizado

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  nome1: string;
+  nome2: string;
+  porcentagem: number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  calcular(){
+    const urlBase = 'http://lucasreno.kinghost.net/love-calculator/';
+    const url = urlBase + this.nome1 + '/' + this.nome2;
+
+    this.http.get(url).subscribe(resposta => this.porcentagem = +resposta);
+  }
+
+}
